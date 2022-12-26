0da217673441e94dd3f85c8549e1ae39c1df461b
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-12-25 21:05:05 -0300

    começãndo a galeria

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as C from './App.styles'
+
+function App() {
+
+  return (
+    <C.Container>
+      <C.Aria>
+        <C.Header>Galeria de fotos</C.Header>
+      </C.Aria>
+    </C.Container>
+  )
+}
+
+export default App
