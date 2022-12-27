0223dc4bd0865e50b434cda0de69c1ef7e46b4d1
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-12-26 21:58:05 -0300

    configurando o firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import * as C from './App.styles'
+import { useState, useEffect } from 'react';
+import * as C from './App.styles';
+import * as Photos from './services/photos';
+
+import { Photo } from './types/photo'
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
 
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    };
+    getPhotos();
+  },[]);
   return (
     <C.Container>
       <C.Aria>
         <C.Header>Galeria de fotos</C.Header>
+        <C.ScreenWarning>
+          <div className='emiji'></div>
+          <div>loading ...</div>
+        </C.ScreenWarning>
       </C.Aria>
     </C.Container>
   )
